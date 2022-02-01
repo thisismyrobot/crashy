cc9454719e1ccec126073194dc58423e6f612fad
Robert <rwallhead@gmail.com>
2022-02-01 16:51:19 +1100

    Move didn't work, but ping sometimes does

--- python client/hello_sphero.py
+++ python client/hello_sphero.py
@@ -10,10 +10,10 @@ def set_checksum(cmd):
 def main(port='COM1'):
     ping_cmd =  [
         0xff,  # SOP
-        0xff,  # Reset timeout and expect an answer.
+        0xff,  # Reset timeout, expect an answer.
         0x00,  # Device ID 0 - the core.
         0x01,  # Command ID 1 - Ping.
-        0x01,  # Sequence ID.
+        0x05,  # Sequence ID.
         0x01,  # Data length.
         0x00,  # Checksum - to be calculated.
     ]
@@ -22,14 +22,31 @@ def main(port='COM1'):
 
     print(ping_cmd)
 
-#    set_checksum([0xFF, 0xFF, 0x00, 0x01, 0x52, 0x01, 0x00])
-
-
-#    return
-
     with serial.Serial(port, 115200, timeout=5, writeTimeout=5) as ser:
         ser.write(bytearray(ping_cmd))
         print(ser.read(256))
+
+    roll_cmd =  [
+        0xff,  # SOP
+        0xfe,  # Reset timeout, don't expect an answer.
+        0x02,  # Device ID 2 - Sphero.
+        0x30,  # Command ID 30 - Roll.
+        0x01,  # Sequence ID.
+        0x05,  # Data length.
+        0x32,  # Speed.
+        0x00,  # Heading MSB.
+        0x00,  # Heading LSB.
+        0x01,  # State for Go var - normal mode.
+        0x00,  # Checksum - to be calculated.
+    ]
+
+    set_checksum(roll_cmd)
+
+    print(roll_cmd)
+
+    with serial.Serial(port, 115200, timeout=5, writeTimeout=5) as ser:
+        ser.write(bytearray(roll_cmd))
+#        print(ser.read(256))
 
 
 if __name__ == '__main__':
