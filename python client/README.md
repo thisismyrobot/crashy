fc9741a9a7a60914c3d6f857d9e94c52721d0c04
Robert <rwallhead@gmail.com>
2022-02-02 11:05:22 +1100

    Works

--- python client/README.md
+++ python client/README.md
@@ -1,4 +1,4 @@
 Modified version of a very basic tester I wrote (in 2013) for PC -> Sphero
 commands.
 
-00001101-0000-1000-8000-00805f9b34fb
+All it does is move the Sphero a little bit to prove its working.
--- python client/hello_sphero.py
+++ python client/hello_sphero.py
@@ -1,52 +1,67 @@
 import sys
+import time
 
 import serial
 
 
-def set_checksum(cmd):
-    cmd[-1] = ~(sum(cmd[2:-1]) % 256) & 0xFF
+class Sphero2CommandBuilder():
+    def __init__(self, sequence_ids=True):
+        self._sequence_id = 54
+        self._sequence_ids = sequence_ids
+
+    @property
+    def _sequence(self):
+        if self._sequence_ids:
+            self._sequence_id += 1
+        return self._sequence_id
+
+    def _build(self, device_id, command_id, *data, expect_answer=False):
+        cmd = [
+            0xff,  # SOP
+            0xff if expect_answer else 0xfe,  # Reset timeout, maybe expect an answer.
+            device_id,
+            command_id,
+            self._sequence,
+            len(data) + 1  # Data length (including checksum byte).
+        ] + list(data)
+
+        # Checksum
+        cmd += [~(sum(cmd[2:]) % 256) & 0xFF]
+
+        return bytes(cmd)
+
+    def _pack_data(self, *data):
+        pass
+
+    def ping(self):
+        return self._build(0x00, 0x01, expect_answer=True)
+
+    def roll(self, speed, heading=0):
+        speed = max(min(speed, 255), 0)
+        heading = max(min(heading, 359), 0)
+
+        heading_msb = 0
+        heading_lsb = heading
+        if heading_lsb > 255:
+            heading_msb = 1
+            heading_lsb = heading_lsb - 256
+
+        return self._build(0x02, 0x30, speed, heading_msb, heading_lsb, 1)
+
+    def stop(self):
+        return self._build(0x02, 0x30, 0, 0, 0, 0)
 
 
 def main(port='COM1'):
-    ping_cmd =  [
-        0xff,  # SOP
-        0xff,  # Reset timeout, expect an answer.
-        0x00,  # Device ID 0 - the core.
-        0x01,  # Command ID 1 - Ping.
-        0x05,  # Sequence ID.
-        0x01,  # Data length.
-        0x00,  # Checksum - to be calculated.
-    ]
-
-    set_checksum(ping_cmd)
-
-    print(ping_cmd)
+    builder = Sphero2CommandBuilder()
 
     with serial.Serial(port, 115200, timeout=5, writeTimeout=5) as ser:
-        ser.write(bytearray(ping_cmd))
-        print(ser.read(256))
+        ser.write(builder.ping())
+        print(f'Ping response: {ser.read(256)}')
 
-    roll_cmd =  [
-        0xff,  # SOP
-        0xfe,  # Reset timeout, don't expect an answer.
-        0x02,  # Device ID 2 - Sphero.
-        0x30,  # Command ID 30 - Roll.
-        0x01,  # Sequence ID.
-        0x05,  # Data length.
-        0x32,  # Speed.
-        0x00,  # Heading MSB.
-        0x00,  # Heading LSB.
-        0x01,  # State for Go var - normal mode.
-        0x00,  # Checksum - to be calculated.
-    ]
-
-    set_checksum(roll_cmd)
-
-    print(roll_cmd)
-
-    with serial.Serial(port, 115200, timeout=5, writeTimeout=5) as ser:
-        ser.write(bytearray(roll_cmd))
-#        print(ser.read(256))
+        ser.write(builder.roll(30))
+        time.sleep(1)
+        ser.write(builder.stop())
 
 
 if __name__ == '__main__':
