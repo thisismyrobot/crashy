f7d120645c658757f0bec72483c4785700ee1f13
Robert <rwallhead@gmail.com>
2022-02-02 12:08:10 +1100

    This will be the ESP32-CAM app (when it arrives...)

---
+++ esp32-cam/esp32-cam.ino
@@ -0,0 +1,15 @@
+/*
+ * Add https://dl.espressif.com/dl/package_esp32_index.json as an additional board manager URL.
+ * Install esp32 board via the board manager (1.0.6).
+ * Select Heltec_Wifi_Kit_32 as board.
+ * 
+*/
+ void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
