a68ff39286b9de172922cc1bdd502cbff09e7497
Robert <rwallhead@gmail.com>
2022-02-06 09:52:22 +1100

    Working detection of the start of the image, on upload

--- src/cloudflare worker/worker.js
+++ src/cloudflare worker/worker.js
@@ -1,31 +1,48 @@
-addEventListener("fetch", event => {
+addEventListener('fetch', event => {
   event.respondWith(handleRequest(event))
 })
 
-async function handleRequest(event) {
-    const parsedUrl = new URL(event.request.url)
+async function handleRequest (event) {
+  const parsedUrl = new URL(event.request.url)
 
-    if (event.request.method === "POST" && parsedUrl.pathname === "/upload") {
-        return handleUpload(event.request)
-    }
+  if (event.request.method === 'POST' && parsedUrl.pathname === '/upload') {
+    return handleUpload(event.request)
+  }
 
-    if (event.request.method === "GET" && parsedUrl.pathname === "/latest.jpg") {
-        return handleLatest(event.request)
-    }
+  if (event.request.method === 'GET' && parsedUrl.pathname === '/latest.jpg') {
+    return handleLatest(event.request)
+  }
 
-    return new Response("Hello world")
+  return new Response('Hello world')
 }
 
-async function handleUpload(request) {
-    let imageData = await request.arrayBuffer()
-    await NAMESPACE.put("latest", imageData)
-    return new Response("Success!");
+async function handleUpload (request) {
+  let imageData = await request.arrayBuffer()
+  let start = await findFileStartOffset(imageData)
+
+  await NAMESPACE.put('latest', imageData.slice(start, -58))
+  return new Response('Success!')
 }
 
-async function handleLatest(request) {
-    let imageData = await NAMESPACE.get("latest", "arrayBuffer")
-    return new Response(
-        imageData.slice(153, -58),
-        { headers: new Headers([["Content-Type", "image/jpeg"]]) }
-    )
+async function handleLatest (request) {
+  return new Response(
+    await NAMESPACE.get('latest', 'arrayBuffer'),
+    { headers: new Headers([['Content-Type', 'image/jpeg']]) }
+  )
+}
+
+async function findFileStartOffset (imageData) {
+  let decoder = new TextDecoder('utf-8')
+  let start = 0
+  let newlines = 0
+  while (true) {
+    let nextValue = decoder.decode(imageData.slice(start, start + 2))
+    if (nextValue === '\r\n') {
+      newlines += 1
+    }
+    if (newlines === 4) {
+      return start + 2
+    }
+    start += 1
+  }
 }
